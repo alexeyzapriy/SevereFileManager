3cd179c47a01e3e87059f396fac86717e9a55eb2
azapriy <alexey.zapriy@gmail.com>
2016-07-09 21:06:10 +0300

    Replace RecycleView with ListView

--- app/src/main/java/com/roman/zapriy/severefilemanager/OnLongClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.roman.zapriy.severefilemanager;
-
-import com.roman.zapriy.severefilemanager.content_for_list.AbstractFileModel;
-
-public interface OnLongClickListener {
-    void onLongClick(AbstractFileModel item);
-}
